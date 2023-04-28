18712fadfa95f7add761ac6fdc774723370ffb15
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-04-28 09:12:21 -0300

    add database Padaria

--- backend/authors/models.py
+++ backend/authors/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 
 class User(AbstractUser):
@@ -7,6 +7,21 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Padaria(AbstractUser):
+    fantasy_name = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=18, unique=True)
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+    groups = models.ManyToManyField(Group, related_name='padaria_groups', blank=True)  # noqa: E501
+    user_permissions = models.ManyToManyField(Permission, related_name='padaria_permissions', blank=True)  # noqa: E501
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []

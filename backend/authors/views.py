13b6c730a67284dce7ad60de32bf2b631adba005
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-04-28 09:14:20 -0300

    update and add Padaria

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PadariaSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
-from .models import User
+from .models import User, Padaria
 import jwt, datetime
 
 
-class RegisterView(APIView):
+class Register_UserView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -14,7 +14,15 @@ class RegisterView(APIView):
         return Response(serializer.data)
 
 
-class LoginView(APIView):
+class Register_PadariaView(APIView):
+    def post(self, request):
+        serializer = PadariaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class Login_UserView(APIView):
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
@@ -43,6 +51,35 @@ class LoginView(APIView):
         return response
 
 
+class Login_PadariaView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        padaria = Padaria.objects.filter(email=email).first()
+
+        if padaria is None:
+            raise AuthenticationFailed('Usuario nao encontrado!')
+
+        if not padaria.check_password(password):
+            raise AuthenticationFailed('Senha incorreta!')
+
+        payload = {
+            'id': padaria.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            "jwt": token
+        }
+        return response
+
+
 class UserView(APIView):
     def get(self, request):
         token = request.COOKIES.get('jwt')
@@ -57,6 +94,24 @@ class UserView(APIView):
 
         user = User.objects.filter(id=payload['id']).first()
         serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+
+class PadariaView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Não Autenticado!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms='HS256')
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Não Autenticado!')
+
+        padaria = Padaria.objects.filter(id=payload['id']).first()
+        serializer = PadariaSerializer(padaria)
 
         return Response(serializer.data)
 

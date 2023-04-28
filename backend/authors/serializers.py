88994b1206e2a98af2eed7a2c105c599bc4fcd3a
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-04-28 09:13:14 -0300

    update serializers

--- backend/authors/serializers.py
+++ backend/authors/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Padaria
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+
+class PadariaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Padaria
+        fields = ['id', 'fantasy_name', 'cnpj', 'telefone',
+                  'endereco', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }

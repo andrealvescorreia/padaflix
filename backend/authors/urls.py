dc4915a198272a8dac7af26cb999273978965bd1
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-05-02 07:33:40 -0300

    Update urls

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import Register_UserView, Register_PadariaView, Login_UserView, Login_PadariaView, UserView, PadariaView, LogoutView  # noqa: E501
+from .views import home, Register_UserView, Register_PadariaView, LoginView, UserView, PadariaView, LogoutView  # noqa: E501
 
 urlpatterns = [
+  path('', home, name='home'),
   path('register_user', Register_UserView.as_view()),
   path('register_padaria', Register_PadariaView.as_view()),
-  path('login_user', Login_UserView.as_view()),
-  path('login_padaria', Login_PadariaView.as_view()),
+  path('login', LoginView.as_view()),
+  # path('login_padaria', Login_PadariaView.as_view()),
   path('user', UserView.as_view()),
   path('padaria', PadariaView.as_view()),
   path('logout', LogoutView.as_view()),

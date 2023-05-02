9c07e0116e8241d939c218594a9450582513cd07
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2023-05-01 22:52:40 -0300

    Feature/frontend/tela listagem de padarias + navbar (#19)
    * Navbar
    
    * npm i react icons --save
    
    * PadariaCard component
    
    * Update main.scss
    
    * /padarias route
    
    * PadariasList

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import axiosInstance from '../../axios'
 import { User } from "../../types/User";
-
+import styles from './Navbar.module.scss';
+import Logo from '../../assets/logo.png';
 
 interface NavBarProps {
   user: User | undefined,
@@ -18,43 +19,56 @@ const NavBar = ( {user, setUser}: NavBarProps ) => {
     })
   }
 
-  let navOptions;
+  let userNavOption;
 
   if (user) { // Caso autenticado
-    navOptions = (
-      <ul className="navbar-nav me-auto mb-2 mb-md-0">
-        <li className="nav-item">
-          <Link to="/login" className="nav-link active" onClick={logout} >Logout</Link>
-        </li>
-      </ul>
+    userNavOption = (
+      <div className={styles.navOptions}>
+        
+        <Link to="/login" id={styles.logout_btn} onClick={logout} >
+          Sair
+        </Link>
+        
+      </div>
     )
   }
   else {// Caso não autenticado...
-    navOptions = (
-      <ul className="navbar-nav me-auto mb-2 mb-md-0">
-        <li className="nav-item">
-          <Link to="/login" className="nav-link active" >Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/register" className="nav-link active" >Register</Link>
-        </li>
-      </ul>
+    userNavOption = (
+      <div className={styles.navOptions}>
+        
+        <Link to="/login"  id={styles.login_btn} className={styles.btn} >
+          Login
+        </Link>
+        
+        <Link to="/register" id={styles.start_now_btn} className={styles.btn} >
+          Começe Agora
+        </Link>
+        
+      </div>
+
     )
   } 
 
   
-
-  return <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div className="container-fluid">
-      <Link to="/" className="navbar-brand">Home</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        {navOptions}
-      </div>
+  return <nav id={styles.navbar}>
+    <div className={styles.defaultNavOptions}>
+      <Link to="/"   className={styles.option} >
+        Início
+      </Link>
+          
+      <Link to="/padarias" className={styles.option} >
+        Padarias
+      </Link>
     </div>
-  </nav>
+
+    <div className={styles.logoContainer}>
+      <img src={Logo} className={styles.logo} alt="logo padaflix"/>
+    </div>
+
+    
+    {userNavOption}
+  </nav>;
+  
 }
  
 export default NavBar;

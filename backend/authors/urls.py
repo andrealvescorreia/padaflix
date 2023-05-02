05e18074127b571bb28336dbae4c28d0e58a36d3
Arthur Medeiros de Oliveira <119054821+ArthurMedeiros29@users.noreply.github.com>
2023-05-02 18:47:49 -0300

    add testes unitarios (#24)

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -3,10 +3,10 @@ from .views import home, Register_UserView, Register_PadariaView, LoginView, Use
 
 urlpatterns = [
   path('', home, name='home'),
-  path('register_user', Register_UserView.as_view()),
-  path('register_padaria', Register_PadariaView.as_view()),
-  path('login', LoginView.as_view()),
-  path('user', UserView.as_view()),
-  path('padaria', PadariaView.as_view()),
-  path('logout', LogoutView.as_view()),
+  path('register_user', Register_UserView.as_view(), name='register_user'),
+  path('register_padaria', Register_PadariaView.as_view(), name='register_padaria'),
+  path('login', LoginView.as_view(), name='login'),
+  path('user', UserView.as_view(), name='user'),
+  path('padaria', PadariaView.as_view(), name='padaria'),
+  path('logout', LogoutView.as_view(), name='logout'),
 ]

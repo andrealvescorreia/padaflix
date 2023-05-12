431136ea11417a270ac87703a2502268edcce0c8
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-05-12 15:24:08 -0300

    add endereço

--- backend/authors/serializers.py
+++ backend/authors/serializers.py
@@ -1,18 +1,29 @@
 from rest_framework import serializers
-from .models import User, Padaria
+from .models import User, Padaria, Endereco
+
+
+class EnderecoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Endereco
+        fields = ['cep', 'rua', 'numero', 'bairro', 'complemento', 'uf']
 
 
 class UserSerializer(serializers.ModelSerializer):
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password']
+        fields = ['id', 'name', 'endereco', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
+        endereco_data = validated_data.pop('endereco')
+        endereco = Endereco.objects.create(**endereco_data)
+
         password = validated_data.get('password', None)
-        instance = self.Meta.model(**validated_data)
+        instance = self.Meta.model(**validated_data, endereco=endereco)
         if password is not None:
             instance.set_password(password)
 
@@ -23,14 +34,18 @@ class UserSerializer(serializers.ModelSerializer):
 class PadariaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Padaria
-        fields = ['id', 'nome_fantasia', 'cnpj', 'telefone', 'email', 'password']  # noqa: E501
+        fields = ['id', 'nome_fantasia', 'endereco',
+                  'cnpj', 'telefone', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
+        endereco_data = validated_data.pop('endereco')
+        endereco = Endereco.objects.create(**endereco_data)
+
         password = validated_data.get('password', None)
-        instance = self.Meta.model(**validated_data)
+        instance = self.Meta.model(**validated_data, endereco=endereco)
         if password is not None:
             instance.set_password(password)
 

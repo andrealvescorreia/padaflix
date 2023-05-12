b738ede4563e672686711a73a5924b3384cd4419
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-05-11 21:50:39 -0300

    interface PadariaUser

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -3,3 +3,19 @@ export interface User {
   name: string,
   email: string,
 }
+
+export interface PadariaUser {
+  id: number,
+  nome_fantasia: string,
+  email: string,
+  cnpj: string,
+  telefone: string,
+}
+
+export function isPadariaUser(object: any): object is PadariaUser {
+  return 'nome_fantasia' in object;
+}
+
+export function isUser(object: any): object is User {
+  return 'name' in object;
+}

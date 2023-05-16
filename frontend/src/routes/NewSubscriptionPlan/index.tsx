9234f91e66ac9491b3299e39bfac727be365713d
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-05-16 11:58:22 -0300

    form

--- frontend/src/routes/NewSubscriptionPlan/index.tsx
+++ frontend/src/routes/NewSubscriptionPlan/index.tsx
@@ -1,15 +1,65 @@
-import { useEffect, useState } from "react";
+import * as React from 'react';
+import { useState, SyntheticEvent } from "react";
+import { Button, Container, TextField } from "@mui/material";
+import "./styles.scss";
+import FormControl from '@mui/material/FormControl';
 
 const NewSubscriptionPlan = () => {
-  const [planName, setPlanName] = useState('');
-  const [planDescription, setPlanDescription] = useState('');
-  const [planValue, setplanValue] = useState(0);
+    const [planName, setPlanName] = useState('');
+    const [planDescription, setPlanDescription] = useState('');
+    const [planValue, setPlanValue] = useState(0);
 
+    
+    const handleSubmit = (e: SyntheticEvent) => {
+        e.preventDefault()
+    }
 
-  return (
-    <div>
-      Ola
-    </div>
+    return (
+        <Container id="new-subscription-plan" maxWidth="sm" >
+            <FormControl variant="standard"  onSubmit={handleSubmit} 
+                sx={{
+                    width: '100%',
+                    maxWidth: '100%',
+                }}
+                component="form"
+                noValidate
+                
+                autoComplete="off"
+            >
+
+                <TextField 
+                    label="Nome" 
+                    value={planName}
+                    onChange={e => setPlanName(e.target.value)} 
+                    autoComplete='off'
+                    margin="dense"
+                />
+                <TextField 
+                    label="Descricao" 
+                    value={planDescription}
+                    onChange={e => setPlanDescription(e.target.value)} 
+                    autoComplete='off'
+                    multiline
+                    margin="dense"
+                />
+                <TextField 
+                    label="Preço" 
+                    value={planValue}
+                    type="number"
+                    onChange={e => setPlanValue(parseInt(e.target.value))} 
+                    autoComplete='off'
+                    margin="dense"
+                />
+
+                <Button 
+                    variant="contained" 
+                    type="submit"
+                    className='submit bttn'
+                >Criar
+                </Button>
+            </FormControl>
+
+        </Container>
   );
 }
  

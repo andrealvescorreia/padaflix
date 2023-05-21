b77029ef4736f7047cdce2393853488682f8bcd4
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-05-21 07:52:19 -0300

    Adicionado máscara telefone e cnpj

--- frontend/src/components/RegisterBakeryForm/index.tsx
+++ frontend/src/components/RegisterBakeryForm/index.tsx
@@ -1,6 +1,7 @@
 import { SyntheticEvent, useState } from "react";
 import { Button, TextField } from "@mui/material";
 import InputPassRegister from "../InputPassRegister";
+import InputMask from 'react-input-mask';
 
 interface RegisterProps {
     onSubmit: (nome_fantasia: string, email: string, password: string, cnpj: string, telefone: string) => void;
@@ -25,21 +26,36 @@ const RegisterBekery = (props: RegisterProps) => {
         <main id = "mainContainer">
         
         <form onSubmit={handleSubmit} id = "secondaryContainer">
+
+                
+
                 <label htmlFor="">Nome fantasia
                     <TextField onChange={e => setName(e.target.value)}/>
                 </label>
 
-                <label htmlFor="">CNPJ
-                    <TextField onChange={e => setCnpj(e.target.value)}/>
-                </label>
+                <InputMask 
+                    mask="99.999.999/9999-99"  
+                    value={cnpj}
+                    onChange={e => setCnpj(e.target.value)}
+                >
+                    <label htmlFor="">CNPJ
+                        <TextField required/>
+                    </label>
+                </InputMask>
 
                 <label htmlFor="">E-mail
                     <TextField onChange={e => setEmail(e.target.value)}/>
                 </label>
 
-                <label htmlFor="">Telefone
-                    <TextField onChange={e => setTelefone(e.target.value)} />
-                </label>
+                <InputMask 
+                    mask="(99) 9 9999-9999"  
+                    value={telefone}
+                    onChange={e => setTelefone(e.target.value)}
+                >
+                    <label htmlFor="">Telefone
+                        <TextField required/>
+                    </label>
+                </InputMask>
 
                 <label htmlFor="">Senha
                     <InputPassRegister onChange={e => setPassword(e.target.value)} />

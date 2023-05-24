a9be42e45a1c360f5f8d0c79c388bcd42fcbc5f2
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-05-24 16:06:38 -0300

    refatoracao

--- frontend/src/routes/RegisterUser/index.tsx
+++ frontend/src/routes/RegisterUser/index.tsx
@@ -8,22 +8,20 @@ import AddressForm from "../../components/AddressForm";
 
 const RegisterUser = () => {
     
-    const [currentRegisterStep, setCurrentRegisterStep] = useState(1)
-
-    interface UserRegisterStep1 {
+    interface UserRegisterStepOne {
         name: string,
         email: string,
         password: string,
     }
     
-    interface UserRegister{
+    interface UserRegisterData {
         name: string,
-        endereco: Endereco,
         email: string,
         password: string,
+        endereco: Endereco,
     }
 
-    const defaultUser : UserRegister = {
+    const defaultUser : UserRegisterData = {
         name: '',
         endereco: {
             cep: '', 
@@ -38,17 +36,19 @@ const RegisterUser = () => {
         password: '',
     }
 
-    const [userRegisterData, setUserRegisterData] = useState<UserRegister>(defaultUser)
+    const [userRegisterData, setUserRegisterData] = useState<UserRegisterData>(defaultUser)
+    const [currentRegisterStep, setCurrentRegisterStep] = useState(1)
+
 
     const navigate = useNavigate()
-    const registerUser = async (user: UserRegister) => {
+    const registerUser = async (user: UserRegisterData) => {
         axiosInstance.post('/register_user', user)
         .then(() => {
             alert('Registrado com sucesso!')
             navigate('/login')
         })
         .catch((err) => {
-            if(!err.response) alert('Servidor fora do ar!')
+            if(!err.response) alert('Servidor do padaflix está fora do ar!')
             else{
                 alert('deu ruim, dê uma olhada no console pra tentar se salvar')
                 console.log(err.response.data)
@@ -56,44 +56,35 @@ const RegisterUser = () => {
         })
     }
     
-    function goToNextRegisterStep(newUserData : UserRegisterStep1){
-        setUserRegisterData(prevUserRegisterData => ({...prevUserRegisterData, ...newUserData}));
+    function goToNextRegisterStep(newUserData : UserRegisterStepOne){
+        setUserRegisterData(prevUserRegisterData => ( {...prevUserRegisterData, ...newUserData} ));
         setCurrentRegisterStep(2)
     }
-    
 
     function goToPreviousRegisterStep(userEndereco: Endereco){
-        setUserRegisterData(prevUserRegisterData => (
-            {
-                ...prevUserRegisterData, 
-                endereco: userEndereco
-            }
-        ));
+        setUserRegisterData(prevUserRegisterData => ( {...prevUserRegisterData, endereco: userEndereco} ));
         setCurrentRegisterStep(1)
     }
     
     function finishRegister(userEndereco: Endereco){
- 
-        setUserRegisterData(prevUserRegisterData => (
-            {
-                ...prevUserRegisterData, 
-                endereco: userEndereco
-            }
-        ))
-
-        registerUser({
-            ...userRegisterData, 
-            endereco: userEndereco
-        } as UserRegister)
+        setUserRegisterData(prevUserRegisterData => ( {...prevUserRegisterData, endereco: userEndereco} ));
+        registerUser( {...userRegisterData, endereco: userEndereco} as UserRegisterData )
     }
 
-
-
-    if(currentRegisterStep == 1){
-        return <RegisterUserForm onSubmit={goToNextRegisterStep} defaultData={userRegisterData}/>
-    }
-    else {
-        return <AddressForm onSubmit={finishRegister} onGoBack={goToPreviousRegisterStep} defaultData={userRegisterData.endereco}/>
+    switch(currentRegisterStep) {
+        case 1:
+            return(
+                <RegisterUserForm 
+                onSubmit={goToNextRegisterStep} 
+                defaultData={userRegisterData}/>
+            )
+        case 2:
+            return(
+                <AddressForm 
+                onSubmit={finishRegister} 
+                onGoBack={goToPreviousRegisterStep} 
+                defaultData={userRegisterData.endereco}/>
+            )
     }
 }
  

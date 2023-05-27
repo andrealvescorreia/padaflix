299514a5175dfc04beabdd8b144676400dcb54d4
André Alves Correia <andre2013aguabranca@gmail.com>
2023-05-27 17:00:19 -0300

    routesPadaria

---
+++ frontend/src/routes/routesPadaria/HorariosPadaria/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss'
+
+const HorariosPadaria = () => {
+    return (
+        <div id="usuario-padaria-horarios">
+            horarios
+        </div>
+    );
+}
+ 
+export default HorariosPadaria;

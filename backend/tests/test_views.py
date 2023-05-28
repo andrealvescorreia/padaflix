1f5d3b3cdaa2f409b1d487b7aaff239ff9a5e383
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-05-28 15:47:32 -0300

    add tests registro Usuário

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -7,6 +7,70 @@ class TestViews(TestSetUp):
         res = self.client.post(self.register_user_url)
         self.assertEqual(res.status_code, 400)
 
+    def test_user_cannot_register_without_name(self):
+        self.user_data['nome'] = ''
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_with_invalid_cep(self):
+        self.user_data['endereco']['cep'] = "123456789"
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_without_cep(self):
+        self.user_data['endereco']['cep'] = ""
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_with_invalid_rua(self):
+        self.user_data['endereco']['rua'] = ""
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_with_invalid_numero(self):
+        self.user_data['endereco']['numero'] = ""
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_with_invalid_bairro(self):
+        self.user_data['endereco']['bairro'] = ""
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_with_invalid_uf(self):
+        self.user_data['endereco']['uf'] = "Paraíba"
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_cannot_register_without_uf(self):
+        self.user_data['endereco']['uf'] = ""
+
+        res = self.client.post(
+            self.register_user_url, self.user_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
     def test_user_can_register_correctly(self):
         res = self.client.post(
             self.register_user_url, self.user_data, format="json"

08a750ddba723f282649024d437ec375f785f525
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-05-28 16:22:45 -0300

    add more test register padaria

--- backend/tests/test_register_padaria.py
+++ backend/tests/test_register_padaria.py
@@ -6,6 +6,166 @@ class TestRegisterPadaria(TestSetUp):
         res = self.client.post(self.register_padaria_url)
         self.assertEqual(res.status_code, 400)
 
+    def test_padaria_cannot_register_without_name(self):
+        self.padaria_data['nome_fantasia'] = ''
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_cep(self):
+        self.padaria_data['endereco']['cep'] = "123456789"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_without_cep(self):
+        self.padaria_data['endereco']['cep'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_rua(self):
+        self.padaria_data['endereco']['rua'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_numero(self):
+        self.padaria_data['endereco']['numero'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_bairro(self):
+        self.padaria_data['endereco']['bairro'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_uf(self):
+        self.padaria_data['endereco']['uf'] = "Paraíba"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_without_uf(self):
+        self.padaria_data['endereco']['uf'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_cnpj(self):
+        self.padaria_data['cnpj'] = "114447770001229"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_without_cnpj(self):
+        self.padaria_data['cnpj'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_telefone(self):
+        self.padaria_data['telefone'] = "839123654780"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_without_telefone(self):
+        self.padaria_data['telefone'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_email_v1(self):
+        self.padaria_data['email'] = "arthurgmailcom"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_email_v2(self):
+        self.padaria_data['email'] = "arthur@gmailcom"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_invalid_email_v3(self):
+        self.padaria_data['email'] = "arthurgmail.com"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_without_email(self):
+        self.padaria_data['email'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_password_less_than_8_digits(self):
+        self.padaria_data['password'] = "Abc12"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_password_without_numbers(self):
+        self.padaria_data['password'] = "Abcdefgh"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_with_non_capitalized_password(self):
+        self.padaria_data['password'] = "abc12345"
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
+    def test_padaria_cannot_register_without_password(self):
+        self.padaria_data['password'] = ""
+
+        res = self.client.post(
+            self.register_padaria_url, self.padaria_data, format="json"
+        )
+        self.assertEqual(res.status_code, 400)
+
     def test_padaria_can_register_correctly(self):
         res = self.client.post(
             self.register_padaria_url, self.padaria_data, format="json"

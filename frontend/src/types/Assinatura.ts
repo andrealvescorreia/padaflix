31759df53ad7d8793604e1df97a055fafc48cffd
André Alves Correia <andre2013aguabranca@gmail.com>
2023-06-18 18:52:56 -0300

    export interface AssinaturaUser

--- frontend/src/types/Assinatura.ts
+++ frontend/src/types/Assinatura.ts
@@ -6,3 +6,15 @@ export interface Assinatura{
     data_inicio: string,
     assinado: boolean,
 }
+
+export interface AssinaturaUser{
+    id: number
+    cliente: number //id do cliente
+    cliente_nome: string
+    plano: number // id do plano
+    nome_plano: string
+    id_padaria: number
+    nome_padaria: string
+    data_inicio: string
+    assinado: boolean
+}

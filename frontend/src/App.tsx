c808f108afb26993a892c911aad0514b4fd70a8a
André Alves Correia <andre2013aguabranca@gmail.com>
2023-06-19 10:13:30 -0300

    refatoracao: rotas separadas por tipo de usuario

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,23 +18,26 @@ import HorariosPadaria from './routes/routesPadaria/HorariosPadaria';
 import PerfilPadaria from './routes/routesPadaria/PerfilPadaria';
 import EnderecoPadaria from './routes/routesPadaria/EnderecoPadaria';
 import PadariaProfile from './routes/PadariaProfile';
-import { useSnackbar } from 'notistack';
+import { enqueueSnackbar } from 'notistack';
 import PadariasList from './routes/PadariasList';
 
 function App() {
   const [user, setUser] = useState<User | PadariaUser | undefined>()
-  const { enqueueSnackbar } = useSnackbar();
-  
+  const navigate = useNavigate()
+
   const fetchUser = async () => {
     axiosInstance.get('/user')
     .then((response) => {
       setUser(response.data);
     })
     .catch((err) => {
+      setUser(undefined)
       if(!err.response) {
         enqueueSnackbar('Servidor do padaflix fora do ar', { variant: 'error'})
       }
-      setUser(undefined)
+      else {
+        console.log(err.response.data)
+      }
     })
   }
 
@@ -45,53 +48,88 @@ function App() {
 
   const logout = async () => {
     axiosInstance.post('/logout')
-      .then(() => {
-        setUser(undefined)
-      })
-      .catch((err) => {
-        console.log(err.response.data)
-      })
+    .then(() => {
+      setUser(undefined)
+    })
+    .catch((err) => {
+      enqueueSnackbar('Ocorreu um erro', { variant: 'error'})
+      console.log(err.response.data)
+    })
   }
   
-  const navigate = useNavigate()
+  
   function onSuccessfulLogin(){
     fetchUser()
     navigate('/inicio')
   }
+
+  function notLoggedInRoutes(){
+    return <>
+      <Route path="/login"                 element={<Login onSuccessfulLogin={onSuccessfulLogin} />} />
+      <Route path="/choose-profile"        element={<ChooseProfile />} />
+      <Route path="/register/user"         element={<RegisterUser/>} />
+      <Route path="/register/user-padaria" element={<RegisterBakery />} />
+    </>
+  }
+
+  function userRoutes(){
+    return <>
+      <Route path="/padarias"     element={<PadariasList user={user} />} />
+      <Route path="/padarias/:id" element={<PadariaProfile user={user} afterSuccessfulSubscription={fetchUser} />}/>
+    </>
+  }
+
+  function padariaRoutes(){
+    return <>
+      <Route path="/padaria-planos"     element={<PlanosPadaria padaria={user} />} />
+      <Route path="/padaria-planos/new" element={<NewSubscriptionPlan />} />
+      <Route path="/padaria-assinantes" element={<AssinantesPadaria padariaUser={user} />} />
+      <Route path="/padaria-avaliacoes" element={<AvaliacoesPadaria />} />
+      <Route path="/padaria-horarios"   element={<HorariosPadaria />} />
+      <Route path="/padaria-perfil"     element={<PerfilPadaria />} />
+      <Route path="/padaria-endereco"   element={<EnderecoPadaria />} />
+    </>
+  }
+
+ 
 
   return (
     <div>
       {
         isPadariaUser(user) 
         ? <SideBarPadaria padaria={user} logout={logout}/>  
-        : <NavBar user={user} logout={logout} />
+        : <NavBar         user={user}    logout={logout} />
       }
       <Routes>
-        <Route path="/inicio" element={<Home user={user} />} />
-        <Route path="/padarias" element={<PadariasList user={user} />} />
-        <Route path="/login" element={<Login onSuccessfulLogin={onSuccessfulLogin} />} />
-        <Route path="/choose-profile" element={<ChooseProfile />} />
-        <Route path="/register/user" element={<RegisterUser/>} />
-        <Route path="/register/user-padaria" element={<RegisterBakery />} />
-        
-        <Route
-          path="/padarias/:id"
-          element={<PadariaProfile user={user} afterSuccessfulSubscription={fetchUser} />}
-        />
-
-        
-
-        <Route path="/padaria-planos" element={<PlanosPadaria padaria={user} />} />
-        <Route path="/padaria-planos/new" element={<NewSubscriptionPlan />} />
-        <Route path="/padaria-assinantes" element={<AssinantesPadaria padariaUser={user} />} />
-        <Route path="/padaria-avaliacoes" element={<AvaliacoesPadaria />} />
-        <Route path="/padaria-horarios" element={<HorariosPadaria />} />
-        <Route path="/padaria-perfil" element={<PerfilPadaria />} />
-        <Route path="/padaria-endereco" element={<EnderecoPadaria />} />
-
+        <Route path="/" element={<DefaultRoute/>}/>
+        <Route path="/inicio" element={ <Home user={user} /> }/>
+        {
+          !user &&
+          notLoggedInRoutes()
+        }
+        {
+          isUser(user) &&
+          userRoutes()
+        }
+        {
+          isPadariaUser(user) &&
+          padariaRoutes()
+        }
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   )
+}
+function PageNotFound() {
+  return <h2>404 Página não encontrada</h2>
+}
+
+function DefaultRoute() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/inicio')
+  }, [])
+  return <></>
 }
 
 export default App

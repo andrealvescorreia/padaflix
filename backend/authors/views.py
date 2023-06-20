35507e4fb422e75e527e2d6b3947c80ff2e81359
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-06-20 12:49:32 -0300

    samesite=None

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -127,7 +127,8 @@ class LoginView(APIView):
 
         response = Response()
 
-        response.set_cookie(key='jwt', value=token, httponly=True, secure=True)
+        response.set_cookie(key='jwt', value=token, httponly=True, secure=True,
+                            samesite=None)
         response.data = {
             "jwt": token
         }

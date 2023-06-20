3aa542c2a01786f2a5ef324edbfd0e29d7fb2660
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-06-20 14:53:14 -0300

    domain='localhost'

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -127,7 +127,7 @@ class LoginView(APIView):
 
         response = Response()
 
-        response.set_cookie(key='jwt', value=token, httponly=True, secure=True)
+        response.set_cookie(key='jwt', value=token, httponly=True, secure=True, domain='localhost')
         response.data = {
             "jwt": token
         }

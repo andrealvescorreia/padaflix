fed217295ff738d7dd79d9f1679a9767eb79af6e
André Alves Correia <andre2013aguabranca@gmail.com>
2023-07-17 10:23:38 -0300

    use localhost

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-//http://18.191.203.223/
-//baseURL: 'http://localhost:8000/api',
+
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: 'https://18.191.203.223:8000/api',
+  //baseURL: 'https://18.191.203.223:8000/api',
+  baseURL: 'http://localhost:8000/api',
   headers: {'Content-Type': 'application/json'}
 });
 

aa4f042d3fb877a6bf17d9e3f5e5ee0482bb0e98
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-09-15 17:51:39 -0300

    baseURL: pythonanywhere
    temp

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  //baseURL: 'https://18.191.203.223:8000/api',
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://andrezitob.pythonanywhere.com/api',
+  //baseURL: 'http://localhost:8000/api',
   headers: {'Content-Type': 'application/json'}
 });
 

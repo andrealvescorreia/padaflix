f40e56482b0e9743f3c5c3d93b4483a2b42f0791
Andre Alves Correia <andre2013aguabranca@gmail.com>
2023-11-23 15:22:11 -0300

    adiciona input de CEP para usuario nao logado

--- frontend/src/routes/PadariasList/index.tsx
+++ frontend/src/routes/PadariasList/index.tsx
@@ -1,10 +1,12 @@
 import axiosInstance from "../../axios";
 import PadariaCard from "../../components/PadariaCard";
-import { PadariaUser, User } from "../../types/User";
+import { PadariaUser, User, isUser } from "../../types/User";
 import "./styles.scss"
 import { useEffect, useState } from "react";
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, TextField } from "@mui/material";
 import { useSnackbar } from 'notistack';
+import InputMask from 'react-input-mask';
+import axios from "axios";
 
 interface Props {
     user: User | PadariaUser | undefined
@@ -16,33 +18,119 @@ interface Padaria {
 
 const PadariasList = ({user} : Props) => {
     const [padarias, setPadarias] = useState<Padaria[]> ([]);
-    const [doneFetching, setDoneFetching] = useState(false)
-    const { enqueueSnackbar } = useSnackbar();
+    const [isFetching, setIsFetching] = useState<boolean>(false)
+    const [isDoneFetching, setIsDoneFetching] = useState<boolean>(false)
 
-    const fetchPadarias = async () => {
-        setDoneFetching(false)
-        axiosInstance.get('/padarias/cep/'+user?.endereco.cep)
+    const [inputCep, setInputCep] = useState('');
+    const [cepIsValid, setCepIsValid] = useState<boolean>()
+
+    const { enqueueSnackbar } = useSnackbar();
+    const [ cidadeAndUf, setCidadeAndUf] = useState('');
+
+    function isCepValid(cep: String){
+        return cep.length == 8
+    }
+
+    const fetchPadarias = async (cep: String) => {
+        
+        setCepIsValid(true)
+        setIsFetching(true)
+        setIsDoneFetching(false)
+        axiosInstance.get('/padarias/cep/'+cep)
         .then((response) => {
             setPadarias(response.data);
         })
         .catch((err) => {
-            enqueueSnackbar("Ocorreu um erro")
             console.log(err.response.data)
+            setPadarias([])
         })
         .finally(()=>{
-            setDoneFetching(true)
+            setIsFetching(false)
+            setIsDoneFetching(true)
         })
-   }
+    }
+
+    interface ViaCepApiResponse {
+        cep: string,
+        logradouro: string, // equivale a rua
+        complemento: string,
+        bairro: string,
+        localidade: string, // equivale a cidade
+        uf: string,
+    }
+
+    
+
+    async function queryCidadeAndUfFromCep(cep: String){
+        return axios.get("https://viacep.com.br/ws/"+ cep +"/json/")
+        .then((response) => {
+            if (response.data.erro) throw 'Bad request'
+            const cepData : ViaCepApiResponse = response.data;
+            return `${cepData.localidade} - ${cepData.uf}`
+        })
+        .catch(() => { throw 'Bad request' })
+    }   
+
+    async function onSubmitInputCep(){
+        if(isCepValid(inputCep) == false) {
+            setCepIsValid(false)
+            return
+        }
+        setCidadeAndUf(await queryCidadeAndUfFromCep(inputCep))
+        fetchPadarias(inputCep)
+    }
 
     useEffect(() => {
-       fetchPadarias()
+        if(isUser(user)) {
+            fetchPadarias(user.endereco.cep)
+            aaa()
+        }
     }, [])
 
+    async function aaa() {
+        if(isUser(user)) {
+        setCidadeAndUf(await queryCidadeAndUfFromCep(user.endereco.cep))
+    }
+    }
+
+    function CepInput() {
+        return <InputMask 
+            mask = "99999-999"  
+            value = {inputCep}
+            onChange = { e => setInputCep((e.target.value).replace('-', '').replaceAll('_', '')) }
+            onBlur = { () => onSubmitInputCep() }
+        >
+            <TextField className="cep-input"
+                size="small"
+                label = "CEP" 
+                error = {cepIsValid == false}
+                onKeyDown={(ev) => {
+                    if (ev.key === 'Enter') {
+                        onSubmitInputCep
+                        ev.preventDefault()
+                    }
+                }}
+                helperText= {cepIsValid == false ? 'CEP incorreto' : ' '}
+            />
+        </InputMask>
+    }
 
     return <div id="padarias-list">
-        { !doneFetching ? <LinearProgress className="linear-progress" /> : null}
-        <h2>Padarias</h2>
-
+        { isFetching && <LinearProgress className="linear-progress" /> }
+        <div className="header">
+            { 
+                user == undefined 
+                && CepInput() 
+            }
+            { 
+                isDoneFetching &&
+                <h2>Padarias em  { cidadeAndUf }</h2>}
+            {   
+                user == undefined && !isFetching && inputCep == '' && !isDoneFetching && 
+                <h2>Insira seu CEP</h2> 
+            }
+        </div>
+        
         <div className="grid">
             {
                 padarias?.map(padaria => 
@@ -53,7 +141,7 @@ const PadariasList = ({user} : Props) => {
                 )
             }
             {
-                padarias.length == 0 && doneFetching &&
+                padarias.length == 0 && isDoneFetching && cepIsValid &&
                 <h2>Sem padarias na sua cidade :(</h2>
             }
         </div>
@@ -61,6 +149,3 @@ const PadariasList = ({user} : Props) => {
 }
  
 export default PadariasList;
-
-
-

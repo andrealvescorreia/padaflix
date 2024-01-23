0eb1e8572313d46a5925a579abcf9598ed252a82
André Alves Correia <andre2013aguabranca@gmail.com>
2024-01-22 21:10:41 -0300

    helper text and error when email already in use
    +refactor

--- frontend/src/components/EmailInput/index.tsx
+++ frontend/src/components/EmailInput/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import validator from 'validator';
 import { TextField } from '@mui/material';
 import axiosInstance from '../../axios';
-import { enqueueSnackbar } from 'notistack';
 
 interface EmailInputProps {
   onChange: (value: string, isValid: boolean) => void,
@@ -12,62 +11,57 @@ interface EmailInputProps {
 const EmailInput = (props: EmailInputProps) => {
   const { onChange, value } = props
   const [email, setEmail] = useState(value);
-  const [isValid, setIsValid] = useState(false);
-  const [touched, setTouched] = useState(false);
-  const [emailJaCadastrado, setEmailJaCadastrado] = useState(false)
+  const [error, setError] = useState(false);
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [helperText, setHelperText] = useState('');
 
   useEffect(() => {
-    const isEmailValid = validator.isEmail(email);
-    setIsValid(isEmailValid);
-    onChange(email, isEmailValid);
-  }, [email, onChange]);
+    onChange(email, emailIsValid);
+  }, [email, emailIsValid])
 
-  const handleEmailChange = (event: React.FocusEvent<HTMLInputElement>) => {
-    const newEmail = event.target.value;
-    setEmail(newEmail);
+  const validateEmail = async (newEmail: string) => {
+    setError(false);
+    setHelperText('');
     const isEmailValid = validator.isEmail(newEmail);
-    setIsValid(isEmailValid);
+    const isEmailInUse = await isEmailAlreadyInUse(newEmail);
+    if (newEmail !== '') {
+      if (!isEmailValid) {
+        setError(true);
+        setHelperText('E-mail inválido');
+      }
+      if (isEmailInUse) {
+        setError(true);
+        setHelperText('E-mail já está em uso');
+      }
+    }
+    return (isEmailValid && !isEmailInUse);
   };
 
-  const handleBlur = () => {
-    setTouched(true);
-  };
-  const handleFocus = () => {
-    setTouched(false)
-  }
-
-  const checkEmail = async () => {
+  async function isEmailAlreadyInUse(email: string) {
     try {
       const response = await axiosInstance.get(`/register_user?email=${email}`);
-      console.log(response.status);
-      if (response.status === 204) {
-        // Email não cadastrado
-        console.log('Email disponível');
-      }
-    } catch (error) {
-      console.error('Ocorreu um erro:', error);
-      enqueueSnackbar(
-        "O e-mail já existe",
-        { variant: "error" }
-      );
+      if (response.status === 204) return false;
+      else return true;
+    } catch (e) {
+      return true;
     }
+    // isso ficou estranho, mas é culpa do backend que retorna um erro caso o email exista!
   };
-  
-  useEffect(() => {
-    if (touched && isValid && email !== '') {
-      checkEmail();
-    }
-  }, [touched, isValid, email]);
+
+  const handleBlur = async (event: React.FocusEvent<HTMLInputElement>) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    setEmailIsValid(await validateEmail(newEmail));
+  };
 
   return (
     <TextField
       value={email}
-      onChange={handleEmailChange}
       onBlur={handleBlur}
-      onFocus={handleFocus}
       required
-      error={touched && !isValid && email != ''}
-      helperText={(touched && !isValid && email != '') ? 'Email inválido' : ''}
+      onChange={e => setEmail(e.target.value)}
+      error={error}
+      helperText={helperText}
     />
   );
 };
